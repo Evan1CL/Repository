19db802eae4707ea514745a0974364505acfcc4c
Student Evan Clail (TL_KCOMP_B_Y1) <t00295941@mymtu.ie>
2026-09-24 09:29:54 +0100

    Added omni directional movement controls
    Signed-off-by: Student Evan Clail (TL_KCOMP_B_Y1) <t00295941@mymtu.ie>

--- Assets/SourceFiles/Scripts/CubeControl.cs
+++ Assets/SourceFiles/Scripts/CubeControl.cs
@@ -12,6 +12,31 @@ public class CubeControl : MonoBehaviour
     void Update()
     {
         // s = u t          s distance = u (velocity) * t (time)
-        transform.position += 0.01f * Vector3.forward;
+        if (Input.GetKey(KeyCode.UpArrow)) 
+        {
+            transform.position += 0.01f * Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            transform.position += 0.01f * Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) 
+        {
+            transform.position += 0.01f * Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            transform.position += 0.01f * Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.LeftControl)) 
+        {
+            transform.position += 0.01f * Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            transform.position += 0.01f * Vector3.up;
+        }
+
+
     }
 }
